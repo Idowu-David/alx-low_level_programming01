1387d37d39ac303f1de86de25e06ef7fabedc47b
Idowu-David <idowudavidodun@gmail.com>
2022-06-04 02:18:36 +0100

    Copied

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,40 +1,28 @@
 #include <stdio.h>
 /**
- * main - prints all possible combinations of 3 numbers
+ * main - Prints possible combinations of three different digits.
+ *
  * Return: Returns 0, Success.
  */
 int main(void)
 {
 	int num1, num2, num3;
 
-	for (num1 = 48; num1 <= 55; num1++)
+	for (num1 = 0; num1 < 8; num1++)
 	{
-		for (num2 = 49; num2 <= 56; num2++)
+		for(num2 = num1 + 1; num2 < 9; num2++)
 		{
-			for (num3 = 50; num3 <= 57; num3++)
+			for (num3 = num2 + 1; num3 < 10; num3++)
 			{
-				if (num1 != num2 || num1 != num3 || num2 != num3)
-				{
-					if (num1 > num2 || num2 > num3)
-					{
-						continue;
-					}
-					else if (num1 == num2 || num2 == num3)
-					{
-						continue;
-					}
-					else
-					{
-						putchar(num1);
-						putchar(num2);
-						putchar(num3);
-						if (num1 != 55)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
+
+				if (num1 == 7 && num2 == 8 && num3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
