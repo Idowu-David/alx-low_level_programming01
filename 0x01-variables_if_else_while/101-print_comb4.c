1b68af6ee583b65f11779a14c1278a28e0b9f689
Idowu-David <idowudavidodun@gmail.com>
2022-06-04 02:06:16 +0100

    Fixed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
 /**
- * main - prints all possible combinations
- * of three digits.
- *
+ * main - prints all possible combinations of 3 numbers
  * Return: Returns 0, Success.
  */
 int main(void)
 {
-	/* define three variables to use as the digits*/
 	int num1, num2, num3;
-	/* loop through the first digits */
+
 	for (num1 = 48; num1 <= 55; num1++)
 	{
-		/* loop through the second digit */
 		for (num2 = 49; num2 <= 56; num2++)
 		{
-			/* loop through the third digit */
 			for (num3 = 50; num3 <= 57; num3++)
 			{
-				/* condition, if none of the numbers are equal*/
 				if (num1 != num2 || num1 != num3 || num2 != num3)
 				{
-					/*conditon, if num3 is the lease, continue, if true.*/
 					if (num1 > num2 || num2 > num3)
 					{
 						continue;
@@ -30,12 +23,11 @@ int main(void)
 					{
 						continue;
 					}
-					/*if none of the conditions are met, print the digits*/
 					else
 					{
-						putchar(num1);/*print the first digit*/
-						putchar(num2);/*print the second digit*/
-						putchar(num3);/*print the third digit*/
+						putchar(num1);
+						putchar(num2);
+						putchar(num3);
 						if (num1 != 55)
 						{
 							putchar(',');
