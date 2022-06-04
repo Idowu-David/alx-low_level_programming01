7ba0efe67d35368554de9bf271d36ac4ef95d866
Idowu-David <idowudavidodun@gmail.com>
2022-06-04 01:48:52 +0100

    Three digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations
+ * of three digits.
+ *
+ * Return: Returns 0, Success.
+ */
+int main(void)
+{
+	/* define three variables to use as the digits*/
+	int num1, num2, num3;
+	/* loop through the first digits */
+	for (num1 = 48; num1 <= 55; num1++)
+	{
+		/* loop through the second digit */
+		for (num2 = 49; num2 <= 56; num2++)
+		{
+			/* loop through the third digit */
+			for (num3 = 50; num3 <= 57; num3++)
+			{
+				/* condition, if none of the numbers are equal*/
+				if (num1 != num2 || num1 != num3 || num2 != num3)
+				{
+					/*conditon, if none of the first and second numbers are equal, continue, if true.*/
+					if (num1 > num2 || num2 > num3)
+					{
+						continue;
+					}
+					/*checks if first digit, second digit or third digits are equal, continue, if true.*/
+					else if (num1 == num2 || num2 == num3)
+					{
+						continue;
+					}
+					/*if none of the conditions are met, print the digits*/
+					else
+					{
+						putchar(num1);/*print the first digit*/
+						putchar(num2);/*print the second digit*/
+						putchar(num3);/*print the third digit*/
+						if (num1 != 55)/*trying not to print , for the last digit*/
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
