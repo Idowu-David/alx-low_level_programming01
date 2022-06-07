c3d31cb5509d09f3f13a0a7efccbb51d00ff11a9
Idowu-David <idowudavidodun@gmail.com>
2022-06-07 06:19:44 -0700

    0._putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints the letters of _putchar
+ *
+ * Return: returns 0, Success.
+ */
+int main(void)
+{
+	char c[10] = "_putchar";
+	int i = 0;
+
+	while (i <= 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
