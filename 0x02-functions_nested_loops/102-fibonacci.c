b51c622ef04d7e8bb49377a047bfa85767ed419b
Idowu-David <idowudavidodun@gmail.com>
2022-06-07 19:18:56 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints the fibonacci numbers
+ * up to 50th term
+ *
+ * Return: Returns 0.
+ */
+int main(void)
+{
+	unsigned long int num[50], i;
+
+	num[0] = 1;
+	num[1] = 2;
+	printf("%ld, %ld,", num[0], num[1]);
+
+	for (i = 2; i <= 48; i++)
+	{
+		num[i] = num[i - 1] + num[i - 2];
+		printf("%lu", num[i]);
+		if (i <= 47)
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
