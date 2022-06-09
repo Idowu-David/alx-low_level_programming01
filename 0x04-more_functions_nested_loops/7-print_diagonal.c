17a7a67ac37d49ca65af5c433a2a03497fdbda10
Idowu-David <idowudavidodun@gmail.com>
2022-06-09 14:59:35 -0700

    Print a diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,17 +11,17 @@ void print_diagonal(int n)
 
 	for (row = 0; row < n; row++)
 	{
-		if (n > 0)
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else if (n > 0)
 		{
 			for (col = 0; col < row; col++)
 			{
 				_putchar(' ');
 			}
 			_putchar('#');
-			_putchar('\n');
-		}
-		else if (n <= 0)
-		{
 			_putchar('\n');
 		}
 	}
