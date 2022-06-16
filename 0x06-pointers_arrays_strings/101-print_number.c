2833b158005568a73066cd241ec995d132eaf9bf
Idowu-David <idowudavidodun@gmail.com>
2022-06-17 00:39:14 +0100

    Reverse array

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_number - prints an integer.
+ * @n: integer.
+ *
+ * Return: Returns 0, success.
+ */
+void print_number(int n)
+{
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+		if (n > 0)
+		{
+			_putchar(n % 10 + '0');
+		}
+		else
+		{
+			_putchar(-n % 10 + '0');
+		}
+	}
+	else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
+	{
+		_putchar(n % 10 + '0');
+	}
+	else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
+	{
+		_putchar('-');
+		_putchar(-n % 10 + '0');
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+}
