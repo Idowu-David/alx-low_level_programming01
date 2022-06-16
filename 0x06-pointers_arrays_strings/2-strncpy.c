d799267b4c109a03ca71021c839ee4dfcc53c21c
Idowu-David <idowudavidodun@gmail.com>
2022-06-16 01:02:08 +0100

    String compare

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: integer
+ *
+ * Return: returns 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
