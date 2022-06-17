e4870dbfa87e1fa9d4da45ebe1b4a1d373294c16
Idowu-David <idowudavidodun@gmail.com>
2022-06-16 18:10:02 -0700

    String compare function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,57 +1,22 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings
+ * _strcmp -compares two strings
  * @s1: first string
  * @s2: second string
  *
- * Return: Returns the difference between the strings.
+ * Return: Returns the difference between the strings
  */
 int _strcmp(char *s1, char *s2)
 {
-	int n1, n2, i;
+	int i;
 
-	while (s1[n1] != '\0')
+	for (i = 0; s1[i] == s2[i]; i++)
 	{
-		n1++;
-	}
-	while (s2[n2] != '\0')
-	{
-		n2++;
-	}
-	if (n1 == n2)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
+		if (s1[i] == '\0')
 		{
-			if (s1[i] != s2[i])
-			{
-				return (s1[i] - s2[i]);
-			}
-			else if (s1[i] == s2[i])
-			{
-				return 0;
-			}
+			return (0);
 		}
 	}
-	else if (n1 > n2)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-		{
-			if (s1[i] != s2[i])
-			{
-				return (s1[i] - s2[i]);
-			}
-		}
-	}
-	else if (n1 < n2)
-	{
-		for (i = 0; s2[i] != '\0'; i++)
-		{
-			if (s1[i] != s2[i])
-			{
-				return (s1[i] - s2[i]);
-			}
-		}
-	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
