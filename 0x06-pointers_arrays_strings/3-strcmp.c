265b7985d34ed299e26fffca247cdc09edad73c1
Idowu-David <idowudavidodun@gmail.com>
2022-06-17 01:58:30 +0100

    Adjusted

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,48 +9,49 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int n1, n2, i, j;
+	int n1, n2, i;
 
-    while (s1[n1] != '\0')
-    {
-        n1++;
-    }
-    while (s2[n2] != '\0')
-    {
-        n2++;
-    }
-    if (n1 == n2)
-    {
-        for (i = 0; s1[i] != '\0'; i++)
-        {
-            if (s1[i] != s2[i])
-            {
-                return (s1[i] - s2[i]);
-            }
-            else if (s1[i] == s2[i])
-            {
-                return 0;
-            }
-        }
-    }
-    else if (n1 > n2)
-    {
-        for (i = 0; s1[i] != '\0'; i++)
-        {
-            if (s1[i] != s2[i])
-            {
-                return (s1[i] - s2[i]);
-            }
-        }
-    }
-    else if (n1 < n2)
-    {
-        for (i = 0; s2[i] != '\0'; i++)
-        {
-            if (s1[i] != s2[i])
-            {
-                return (s1[i] - s2[i]);
-            }
-        }
-    }
+	while (s1[n1] != '\0')
+	{
+		n1++;
+	}
+	while (s2[n2] != '\0')
+	{
+		n2++;
+	}
+	if (n1 == n2)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			if (s1[i] != s2[i])
+			{
+				return (s1[i] - s2[i]);
+			}
+			else if (s1[i] == s2[i])
+			{
+				return 0;
+			}
+		}
+	}
+	else if (n1 > n2)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			if (s1[i] != s2[i])
+			{
+				return (s1[i] - s2[i]);
+			}
+		}
+	}
+	else if (n1 < n2)
+	{
+		for (i = 0; s2[i] != '\0'; i++)
+		{
+			if (s1[i] != s2[i])
+			{
+				return (s1[i] - s2[i]);
+			}
+		}
+	}
+	return (0);
 }
