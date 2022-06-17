8b28f084447b08084fc566d014b5e19fa14dc037
Idowu-David <idowudavidodun@gmail.com>
2022-06-17 02:58:12 +0100

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string
+ * @s: string
+ *
+ * Return: returns 0, success.
+ */
+char *leet(char *s)
+{
+	char ch1[] = "aeotl";
+	char ch2[] = "AEOTL";
+	char num[] = "43071";
+
+	int i, j;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == ch1[j] || s[i] == ch2[j])
+			{
+				s[i] = num[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
