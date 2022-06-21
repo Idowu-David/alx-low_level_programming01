363db001f222ed31edfd16bff9867dc78cb88534
Idowu-David <idowudavidodun@gmail.com>
2022-06-21 23:45:39 +0100

    Strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ *
+ * Return: the remaining string after locating c.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s != c)
+	{
+		return (NULL);
+	}
+}
