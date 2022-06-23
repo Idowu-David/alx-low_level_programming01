2f24db8c8543dfc0fdc71fb5e0de2e28b0ba250a
Idowu-David <idowudavidodun@gmail.com>
2022-06-23 17:16:19 +0100

    square

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,9 +14,9 @@ int _is_prime(int num, int checker, int i)
 				      * number less than 0 or
 				      * equal to 1 is not prime
 				      */
-		return 0;
+		return (0);
 	if (num == checker) /* if number is same as checker,number is prime */
-		return 1;
+		return (1);
 	i = (num % checker); /**
 			      * get the remainder when
 			      * number divides checker*/
@@ -24,15 +24,15 @@ int _is_prime(int num, int checker, int i)
 		     * if the remainder is not zero, call the
 		     * function recursively to start all over
 		     * increment checker */
-		return (_is_prime(num, checker + 1));
+		return (_is_prime(num, checker + 1, 1));
 	else /**
 	      * if none of the conditions are true, number is not prime
 	      */
-		return 0;
+		return (0);
 }
 
 /**
- * sqrt - finds the square root of a number
+ * _sqrt - finds the square root of a number
  * @n: number
  * @checker: checker
  *
@@ -41,24 +41,24 @@ int _is_prime(int num, int checker, int i)
  * if number has a root.
  */
 
-int sqrt(int n, int checker)
+int _sqrt(int n, int checker)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 	if (n == 1)
-		return 1;
+		return (1);
 	if ((n == checker) && (n != 1))
-		return -1;
+		return (-1);
 	if ((n / checker) == checker)
 	{
 		if (checker * checker == n)
 		{
-			return checker;
+			return (checker);
 		}
 	}
 	else
 	{
-		return (sqrt(n, checker + 1))
+		return (sqrt(n, checker + 1));
 	}
 }
 
@@ -73,8 +73,9 @@ int _sqrt_recursion(int n)
 	int num1, num2;
 
 	num1 = _is_prime(n, 2, 1); /* call the _is_prime func*/
-	num2 = sqrt(n, 1) /* call the sqrt func */
+	num2 = _sqrt(n, 1); /* call the sqrt func */
 	if (num1 == 1)
 		return -1;
-	return num2;
+	return (num2);
+	return (0);
 }
