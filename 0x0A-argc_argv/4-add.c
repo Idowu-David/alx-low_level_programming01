6f7fe22773f7bff59252145a7eeb758e66de14d0
Idowu-David <idowudavidodun@gmail.com>
2022-06-25 00:21:57 +0100

    Fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * main - adds positive numbers
@@ -12,7 +13,7 @@
 
 int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int i, numCheck, sum = 0;
 
 	if (argc == 1)
 		printf("%d\n", 0);
@@ -20,7 +21,8 @@ int main(int argc, char **argv)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) % 10)
+			numCheck = Isnumber(argv[i]);
+			if (numCheck == 1)
 			{
 				sum += atoi(argv[i]);
 			}
@@ -31,6 +33,24 @@ int main(int argc, char **argv)
 			}
 		}
 		printf("%d\n", sum);
+	}
+	return (0);
+}
+
+/**
+ * isnumber - checks if the argument passed is a number
+ * @str: string passed from argument vector.
+ *
+ * Return: returns 1, if success, returns 0, if not a number.
+ */
+int isnumber(const char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (1);
 	}
 	return (0);
 }
