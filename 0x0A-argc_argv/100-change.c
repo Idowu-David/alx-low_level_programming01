820642723d671303c8848486ce3a36f6d932e2ba
Idowu-David <idowudavidodun@gmail.com>
2022-06-25 00:59:46 +0100

    add

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,43 +10,44 @@
 
 int main(int argc, char const **argv)
 {
-    int i = 0, used = 0, coin = 0;
-    int coins[] = {25, 10, 5, 2, 1};
+	int i = 0, used = 0, coin = 0;
+	int coins[] = {25, 10, 5, 2, 1};
 
-    if (argc != 2)
-    {
-	    printf("Error\n");
-	    return (1);
-    }
-    if (isInteger(argv[1]))
-    {
-	    i = atoi(argv[1]);
-	    while (i > 0 && coin <= 4)
-	    {
-		    if (i >= coins[coin])
-		    {
-			    i -= coins[coin];
-			    used++;
-		    }
-		    else
-		    {
-			    coin++;
-		    }
-	    }
-    }
-    printf("%i\n", used);
-    return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (isinteger(argv[1]))
+	{
+		i = atoi(argv[1]);
+		while (i > 0 && coin <= 4)
+		{
+			if (i >= coins[coin])
+			{
+				i -= coins[coin];
+				used++;
+			}
+			else
+			{
+				coin++;
+			}
+		}
+	}
+	printf("%i\n", used);
+	return (0);
 }
 
 /**
  * isInteger - checks if s is an integer
- * @s: string to check
+ * @str: string to check
  * Return: 0 or 1
  */
 
-int isInteger(const char *str)
+int isinteger(const char *str)
 {
 	int i = 0;
+
 	while (str[i] != '\0')
 	{
 		if (str[i] < '0' || str[i] > '9')
