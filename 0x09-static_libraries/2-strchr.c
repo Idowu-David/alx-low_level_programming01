0922aff5c5beae812d7496d3cefecea7f0c92f30
Idowu-David <idowudavidodun@gmail.com>
2022-06-24 13:29:14 +0100

    Static Library

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#define NULL 0
+/**
+ * _strchr - locates the first occurence of a character in a string.
+ * @s: string
+ * @c: character
+ *
+ * Return: pointer to the character in string.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0' && s[i] != c)
+	{
+		i++;
+	}
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
