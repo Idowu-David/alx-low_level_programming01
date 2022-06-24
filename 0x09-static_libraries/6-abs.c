71c7ed91341f262bed383e42b28a76b01cf5917a
Idowu-David <idowudavidodun@gmail.com>
2022-06-24 19:25:18 +0100

    Static lib

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -6,12 +6,11 @@
  */
 int _abs(int n)
 {
-	int n, bool1, bool2, booldiff, abs;
-
-	bool1 = (n > 0);
-	bool2 = (n < 0);
-	booldiff = (bool1 - bool2);
-	abs = (n * booldiff);
-	putchar(abs(n) + '0');
-	return (0);
+	if (n < 0)
+	{
+		n = -n;
+		return n;
+	}
+	else
+		return n;
 }
