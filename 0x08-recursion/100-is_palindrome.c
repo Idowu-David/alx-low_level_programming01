2a0b8af34e985e2c26fa6fc75e221ca6ec45f504
Idowu-David <idowudavidodun@gmail.com>
2022-06-29 01:27:08 +0100

    Reverse string function

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,28 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-
-/** rev_string - reverses a string
- * @s: string.
- *
- * Return: Returns a pointer to the reversed string.
- */
-
-char *rev_string(char *s)
-{
-	int i, len, j;
-	char *rev;
-
-	len = 0;
-	while (s[len] != '\0')
-		len++;
-	rev = (char *)malloc(len * (sizeof(char) + 1));
-	for (i = (len - 1), j = 0; i >= 0; i--, j++)
-	{
-		rev[j] = s[i];
-	}
-	rev[j] = '\0';
-	return (rev);
-}
 
 /**
  * check_pal - checks if the parameter strings are same or not.
