d7fe2980bd54b13a7362975824328cd8420e0784
Idowu-David <idowudavidodun@gmail.com>
2022-07-01 00:55:19 +0100

    String concat adjusted

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * string_nconcat - concatenates two strings.
@@ -12,40 +11,43 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, j, a, len1, sum;
-	unsigned int len2;
+	int i, j, a, sum;
+	unsigned int len1, len2;
 	char *str;
-	char *null = "";
+
 	if (s1 == NULL)
-		s1 = null;
+		s1 = "";
 	if (s2 == NULL)
-		s2 = null;
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+		s2 = "";
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 	sum = len1 + len2;
-	str = malloc((sum + 1) * sizeof(char));
-	/* Check if memory is successfully allocated */
-	if (str == NULL)
-		return NULL;
-	/* Copy s1 into str */
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		str[i] = s1[i];
-	}
-	str[i] = '\0';
-	/* check if the specified byte is greater than the lenght of s2 */
+	/* check if the specified byte is greater than the length of s2 */
 	if (n >= len2)
 	{
+		str = malloc(sum * sizeof(char) + 1);
+		if (str == NULL)
+			return (NULL);
+		/* copy s1 into str */
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			str[i] = s1[i];
+		}
 		/*copy all the strings in s2 into str */
 		for (j = len1, a = 0; s2[a] != '\0'; j++, a++)
 		{
 			str[j] = s2[a];
 		}
-		str[j] = '\0';
+		str[j] = '\0'
 	}
-	/* if the specified string is less than lenght of s2 */
+	/* if the specified string is less than length of s2 */
 	else if (n < len2)
 	{
+		str = malloc((n + len1) * sizeof(char) + 1);
+		if (str == NULL)
+			return (NULL);
 		for (j = len1, a = 0; s2[a] != s2[n]; j++, a++)
 		{
 			str[j] = s2[a];
