ab838541d86a727cd64ba0b007108efaa0f413a9
Idowu-David <idowudavidodun@gmail.com>
2022-07-01 01:26:28 +0100

    String concat - copy

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,41 +1,51 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * string_nconcat - concatenates two strings.
- * @s1: string (dest)
- * @s2: string (source)
- * @n: number of bytes of string to copy.
- *
- * Return: returns the concatenated string.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i,len1, len2;
-	char *str;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[len1] != '\0')
-		len1++;
-	while (s2[len2] != '\0')
-		len2++;
-	sum = len1 + len2;
-	str = malloc(sum * sizeof(char) + 1);
-	if (str == NULL)
-		return (NULL);
-	/* copy s1 into str */
-	for (i = 0; s1[i] != '\0'; i++)
+
+	while (s1[size1] != '\0')
 	{
-		str[i] = s1[i];
+		size1++;
 	}
-	/*copy all the strings in s2 into str */
-	for (; i < (len1 + n); i++)
+
+	while (s2[size2] != '\0')
 	{
-		str[i] = s2[i - len1];
+		size2++;
 	}
-	str[i] = '\0';
-	return (str);
+
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (0);
+
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+return (p);
 }
