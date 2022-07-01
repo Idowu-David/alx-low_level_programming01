c42e3d5d304e9b66cd4938c64e544c14f5e80828
Idowu-David <idowudavidodun@gmail.com>
2022-07-01 01:44:24 +0100

    Adjust - copy

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 
 /**
+ * fill_mem - fills memory with constant char.
+ * @s: string.
+ * @b: char.
+ * @n: number of bytes.
+ * 
+ * Return: returns a pointer to the filled memory.
+ */
+char *fill_mem(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+/**
  * _calloc - allocates memory for an array.
  * @nmemb: number of elements
  * @size: size of the array.
@@ -16,9 +34,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	array = malloc(nmemb * size);
 	if (array == NULL)
 		return NULL;
-	for (i = 0; i < nmemb; i++)
-	{
-		array[i] = 0;
-	}
+	fill_mem(array, 0, (nmemb * size));
 	return (array);
 }
