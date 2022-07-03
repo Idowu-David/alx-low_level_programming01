e3e2932a1586f0684f9d9ba03e44bb027e2e7189
Idowu-David <idowudavidodun@gmail.com>
2022-07-03 18:22:25 +0100

    redo

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,33 +1,51 @@
 #include "main.h"
 
 /**
- * print_times_table - prints n times table.
- * @n: number
- *
- * Return: void
+ * print_times_table - function that prints the n times table, starting with 0.
+ * @n: input of table n
+ * Return: nothing.
  */
+
 void print_times_table(int n)
 {
-        int i, j;
+	int row, col;
+	int mult = 0;
 
-        if (n > 15 || n < 0)
-        {
-                _putchar('\0');
-        }
-        else
-        {
-                for (i = 0; i < (n + 1); i++)
-                {
-                        for (j = 0; j < (n + 1); j++)
-                        {
-                                _putchar((i * j) + '0');
-                                if (j < n)
-                                {
-                                        _putchar(',');
-                                        _putchar('\t');
-                                }
-                                _putchar('\n');
-                        }
-                }
-        }
+	if (n <= 15 && n >= 0)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (col = 0; col <= n; col++)
+			{
+				mult = (row * col);
+				if (mult < 10)
+				{
+					if (col != 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(mult + '0');
+				}
+				else if (mult < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((mult / 10) + '0');
+					_putchar((mult % 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar((mult / 100) + '0');
+					_putchar(((mult % 100) / 10) + '0');
+					_putchar((mult % 10) + '0');
+				}
+				if (col != n)
+					_putchar(',');
+			}
+			_putchar('\n');
+		}
+	}
 }
