e9c107962fff1916bde3835afc49ba5a8550b55b
Idowu-David <idowudavidodun@gmail.com>
2022-07-04 16:17:22 +0000

    New type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,16 +2,17 @@
 #define DOG_H
 /**
  * struct dog - description of a dog.
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * @name: character string
+ * @age: integer
+ * @owner: character string
  */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
