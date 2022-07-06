f11aa5590832dcb3b076de46412daa5a20a24fef
Idowu-David <idowudavidodun@gmail.com>
2022-07-06 00:44:30 +0000

    New Dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,43 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog.
+ * struct dog - details of dog.
+ * @d: pointer to dog elements
+ *
+ * Return: void.
+ */
+
+void print_dog(struct dog *d)
+{
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+	/**
+	 * print_dog - prints a struct dog.
+	 * struct dog - details of dog.
+	 * @d: pointer to dog elements
+	 *
+	 * Return: void.
+	 */
+
+	void print_dog(struct dog *d)
+	{
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+		/**
+		 * print_dog - prints a struct dog.
+		 * struct dog - details of dog.
+		 * @d: pointer to dog elements
+		 *
+		 * Return: void.
+		 */
+
+		void print_dog(struct dog *d)
+		{
+
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - function for a new dog.
@@ -14,13 +15,28 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *dog;
+	int len1, len2;
 
+	len1  = strlen(name);
+	len2   = strlen(owner);
 	dog = malloc(sizeof(struct dog));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = name;
+	dog->name = malloc(sizeof(char) * len1 + 1);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->name = strcpy(dog->name, name);
+	dog->owner = malloc(sizeof(char) * len2 + 1);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = strcpy(dog->owner, owner);
 	dog->age = age;
-	dog->owner = owner;
-
 	return (dog);
 }
