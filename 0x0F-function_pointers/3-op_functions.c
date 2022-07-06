91a2face89cef53bdd85c25c1bab6113adf849da
Idowu-David <idowudavidodun@gmail.com>
2022-07-06 18:00:26 +0000

    Calculator C

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * op_add - adds the two integers.
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: returns sum of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts the two integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: returns the difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies the two integers.
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: returns the product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides integers.
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: returns the result of the division of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of division of two integers.
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: returns the remainder of the division of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
