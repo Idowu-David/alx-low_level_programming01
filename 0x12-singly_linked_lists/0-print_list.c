c25699966045a6a22ec6d86dfb7377a074a4e866
Idowu-David <idowudavidodun@gmail.com>
2022-07-14 22:22:30 +0000

    Modified print_list function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,21 +10,20 @@
 
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
 		if (h->str != NULL)
 		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			count++;
 		}
 		else
 		{
 			printf("[0] (nil)\n");
-			h = h->next;
 		}
+		count++;
+		h = h->next;
 	}
 	return (count);
 }
