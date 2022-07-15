1a7fa2c7f66da8c6dc9d97030970723f79751c00
Idowu-David <idowudavidodun@gmail.com>
2022-07-15 00:28:40 +0000

    Added function that executes before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * startup - executes a program before main.
+ *
+ * Return: void
+ */
+
+void startup(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
