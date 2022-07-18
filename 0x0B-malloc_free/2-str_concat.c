50442c17a3b93646bc7673b78791b0a976653852
Idowu-David <idowudavidodun@gmail.com>
2022-07-18 15:02:19 +0000

    Modified the variables used

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,41 +1,35 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * str_concat - concatenates two strings.
+ * str_concat - concatenates two strings
  * @s1: first string
- * @s2: second string.
+ * @s2: second string
  *
- * Return: returns the concatenated string.
+ * Return: returns a pointer to a newly allocated string containing contents
+ * of s1 and s2.
+ * returns NULL, if the function fails.
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	int a, i, j, k, n1, n2;
+	int lenA, lenB, index, iterator;
 	char *str;
-	char *null = "";
 
-	n1 = 0;
-	n2 = 0;
 	if (s1 == NULL)
-		s1 = null;
+		s1 = "";
 	if (s2 == NULL)
-		s2 = null;
-	while (s1[n1] != '\0')
-		n1++;
-	while (s2[n2] != '\0')
-		n2++;
-	j = n1 + n2;
-	str = (char *)malloc(j * sizeof(char) + 1);
+		s2 = "";
+	lenA = strlen(s1);
+	lenB = strlen(s2);
+	str = malloc((sizeof(char) * (lenA + lenB)) + 1);
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < n1; i++)
-	{
-		str[i] = s1[i];
-	}
-	for (a = 0, k = n1; s2[a] != '\0'; k++, a++)
-	{
-		str[k] = s2[a];
-	}
-	str[k] = '\0';
+	for (index = 0; s1[index] != '\0'; index++)
+		str[index] = s1[index];
+	for (index = lenA, iterator = 0; s2[iterator] != '\0'; index++, iterator++)
+		str[index] = s2[iterator];
+	str[index] = '\0';
 	return (str);
 }
