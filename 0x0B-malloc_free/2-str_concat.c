b5457263fcae944bb895b91c4990aa2d0052dceb
Idowu-David <idowudavidodun@gmail.com>
2022-07-18 16:07:07 +0100

    Modified 2-strconcat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,8 +1,5 @@
 #include <stdlib.h>
-<<<<<<< HEAD
 #include "main.h"
-=======
->>>>>>> 50442c17a3b93646bc7673b78791b0a976653852
 #include <string.h>
 
 /**
@@ -10,15 +7,9 @@
  * @s1: first string
  * @s2: second string
  *
-<<<<<<< HEAD
  * Return: returns a poiner to a newly allocated space in memory
  * containing the contents of s1 and s2 concatenated, if success.
  * Returns NULL if the function fails.
-=======
- * Return: returns a pointer to a newly allocated string containing contents
- * of s1 and s2.
- * returns NULL, if the function fails.
->>>>>>> 50442c17a3b93646bc7673b78791b0a976653852
  */
 
 char *str_concat(char *s1, char *s2)
@@ -36,19 +27,9 @@ char *str_concat(char *s1, char *s2)
 	if (str == NULL)
 		return (NULL);
 	for (index = 0; s1[index] != '\0'; index++)
-<<<<<<< HEAD
-	{
-		str[index] = s1[index];
-	}
-	for (index = lenA, iterator = 0; s2[iterator] != '\0'; index++, iterator++)
-	{
-		str[index] = s2[iterator];
-	}
-=======
 		str[index] = s1[index];
 	for (index = lenA, iterator = 0; s2[iterator] != '\0'; index++, iterator++)
 		str[index] = s2[iterator];
->>>>>>> 50442c17a3b93646bc7673b78791b0a976653852
 	str[index] = '\0';
 	return (str);
 }
