02141e93c91de88fde388bcd6856b95cd8766ed7
Idowu-David <idowudavidodun@gmail.com>
2022-07-19 15:06:02 +0000

    trucated the malloc return checker

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new = *head;
 	temp = malloc(sizeof(listint_t));
 	if (temp == NULL)
-		return (NULL);
+		exit(1);
 	temp->n = n;
 	temp->next = NULL;
 	if (*head == NULL)
