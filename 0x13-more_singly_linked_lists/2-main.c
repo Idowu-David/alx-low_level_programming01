6c3418167c061e48c57163d822ec37184aee3dc8
Idowu-David <idowudavidodun@gmail.com>
2022-07-19 13:10:19 +0000

    Added addnodeint function

--- 0x13-more_singly_linked_lists/2-main.c
+++ 0x13-more_singly_linked_lists/2-main.c
@@ -8,6 +8,17 @@
  *
  * Return: Always 0.
  */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
 int main(void)
 {
     listint_t *head;
