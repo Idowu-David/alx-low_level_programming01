14e34c45dad7aa6f3d56e7a84a81eb3eac584388
Idowu-David <idowudavidodun@gmail.com>
2022-07-23 22:43:46 +0100

    Adeded cap string function

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,3 +7,34 @@
  * Return: returns the capitalized string.
  */
 
+char *cap_string(char *str)
+{
+	int len, sepLen, index, i; /* i means index2 */
+	char *sep = " \t\n,;.!?\"(){}";
+
+	len = strlen(str);
+	sepLen = strlen(sep);
+	/**
+	 * Loop through the separator, another inner loop in the string,
+	 * check if the separator is same as the index of the string
+	 * if true, check the next index of the string
+	 * if the character in the next index is not uppercase
+	 * change to uppercase with 'str[index] + 32
+	 * repeat the process until done.
+	 */
+
+	for (index = 0; index < sepLen; index++)
+	{
+		for (i = 0; i < len; i++)
+		{
+			if (str[i] == sep[index])
+			{
+				if ((str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+						{
+							s[i + 1] -= 32;
+						}
+			}
+		}
+	}
+	return (str);
+}
