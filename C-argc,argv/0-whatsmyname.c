2589a737322d41b38dee298a7ecbd1d9fa44db35
Idowu-David <idowudavidodun@gmail.com>
2022-07-23 21:52:25 +0000

    Fixed typo

---
+++ C-argc,argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints its name
+ *
+ * Return: returns 0, if success.
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
