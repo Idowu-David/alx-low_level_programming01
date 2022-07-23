5883cfbf58f112c865951b59cd368b42e7f29d38
Idowu-David <idowudavidodun@gmail.com>
2022-07-23 22:00:23 +0000

    Fixed betty erro

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -30,9 +30,7 @@ char *cap_string(char *str)
 			if (str[i] == sep[index])
 			{
 				if ((str[i + 1] >= 'a' && str[i + 1] <= 'z'))
-						{
-							str[i + 1] -= 32;
-						}
+					str[i + 1] -= 32;
 			}
 		}
 	}
