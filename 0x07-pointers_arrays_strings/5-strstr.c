47ea946dc560f371117b2f427124294726ee2317
Idowu-David <idowudavidodun@gmail.com>
2022-07-26 17:15:47 +0100

    Modified _strstr() function

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,47 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * *_strstr - description
- * @haystack: string
- * @needle: pointer
- * Return: pointer
- */
 
+/**
+ * _strstr - locates a substring.
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: a pointer to the beginning of the located substring
+ * or NULL if the substring is not found.
+ *
+ * The _strstr() function finds the first occurence of the substring needle
+ * in the string haystack. The terminating null bytes ('\0') are not compared
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
-int i, j;
+	int len, count, indexA, indexB, flag, index;
 
-for (i = 0; haystack[i] > '\0'; i++)
-{
-	for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
+	flag = 0;
+	len = strlen(needle);
+	if (haystack == NULL)
+		return (NULL);
+	for (indexA = 0; needle[indexA] != '\0'; indexA++)
 	{
-		if (haystack[j] != needle[j - i])
+		for (indexB = 0; haystack[indexB] != '\0'; indexB++)
 		{
-			break;
+			if (needle[indexA] == haystack[indexB])
+			{
+				flag = 1;
+				break;
+			}
 		}
+		if (flag == 1)
+			break;
 	}
-	if (needle[j - i] == '\0')
+	flag = indexB;
+	index = 0;
+	while (needle[index] != '\0' && needle[index] == haystack[flag])
 	{
-		return (haystack + i);
+		count++;
+		index++;
+		flag++;
 	}
-}
-return (0);
+	if (count == len)
+		return (&haystack[indexB]);
+	return (NULL);
 }
