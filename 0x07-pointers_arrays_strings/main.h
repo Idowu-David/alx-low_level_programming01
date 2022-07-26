1be9c6d67cbeb9348398bdac6787051e7ef8cdd9
Idowu-David <idowudavidodun@gmail.com>
2022-07-26 16:25:12 +0000

    Working on memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/* Standard Library */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* Mandatory task function prototypes */
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
