66ddff1b0b83b10b6acbbf5019b1fc5aecfd5068
Idowu-David <idowudavidodun@gmail.com>
2022-08-29 23:57:45 +0000

    fix: a check to insert node at the beginning

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,13 +19,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+	new->n = n;
+	new->next = NULL;
 	len = list_len(*head);
 	if (idx <= len)
 	{
 		/* if no node is in the linked list */
-		if (ptr == NULL)
+		if (*head == NULL)
 		{
-			new->n = n;
 			*head = new;
 			return (new);
 		}
@@ -34,7 +35,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			ptr = ptr->next;
 			idx--;
 		}
-		new->n = n;
 		new->next = ptr->next;
 		ptr->next = new;
 		return (new);
