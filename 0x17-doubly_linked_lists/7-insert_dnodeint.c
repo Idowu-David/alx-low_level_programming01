6ffbfe0bf6505b5f76340b0d6704b57bb527dd7a
Idowu-David <idowudavidodun@gmail.com>
2022-09-02 07:33:38 +0000

    fix: recompiled file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -47,3 +47,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	return (new);
 }
+
+/**
+ * dlistint_len - counts the number of elements in a linked list
+ * @h: pointer to the first node of the list
+ *
+ * Return: the number of elements in a linked list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
