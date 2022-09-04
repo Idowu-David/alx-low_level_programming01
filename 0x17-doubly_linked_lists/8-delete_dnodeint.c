51a23cf2698788bf3ad5a5342bd69dfb2190b77c
Idowu-David <idowudavidodun@gmail.com>
2022-09-04 13:11:18 +0000

    fix: added dlistlen function

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -54,3 +54,21 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	ptr2 = NULL;
 	return (1);
 }
+
+/**
+ * dlistint_len - counts the number of elements in a linked list
+ * @h: pointer to the first node of the list
+ *
+ * Return: the number of elements in a linked list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
