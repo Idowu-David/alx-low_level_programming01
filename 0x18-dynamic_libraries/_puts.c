8ae04945c3cb2804cc61a6e830af7315d8c01933
Idowu-David <idowudavidodun@gmail.com>
2022-09-05 13:09:41 +0000

    fix: betty errors

--- 0x18-dynamic_libraries/_puts.c
+++ 0x18-dynamic_libraries/_puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts - prints a string, followed by a new line,
  * @str: pointer to the string to print
@@ -8,7 +9,7 @@
 void _puts(char *str)
 {
 	int i = 0;
-	
+
 	while (str[i])
 	{
 		_putchar(str[i]);
