110611118262b4aab2f555143042220b173ba945
Idowu-David <idowudavidodun@gmail.com>
2022-09-23 22:28:52 +0000

    fix: condition for nul key, changed new_node to node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,27 +9,27 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
+	hash_node_t *node;
 	unsigned int index;
 	char *Null = "";
 
-	if (key == NULL || key == Null || value == NULL || ht == NULL)
+	if (key == NULL || key == Null || value == NULL || ht == NULL || *key == '\0')
 		return (0);
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 		return (0);
 
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = NULL;
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
 
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL) /* if index is empty */
-		ht->array[index] = new_node;
+		ht->array[index] = node;
 	else	/* add new node at the beginning of the linked list */
 	{
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
+		node->next = ht->array[index];
+		ht->array[index] = node;
 	}
 	return (1);
 }
