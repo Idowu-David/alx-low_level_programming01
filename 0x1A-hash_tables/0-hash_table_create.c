0905b2a6382a9c877257f82fcdd6bbf3cba79d22
Idowu-David <idowudavidodun@gmail.com>
2022-09-23 23:09:45 +0000

    fix: removed trailing whitepace

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 
 	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL) 
+	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
 	new_table->array = malloc(size * sizeof(char *));
