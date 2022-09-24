aa5fea17a916b90a131cf71e6869b5e317651270
Idowu-David <idowudavidodun@gmail.com>
2022-09-24 10:26:56 +0000

    add: print hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,27 +7,51 @@
 void hash_table_print(const hash_table_t *ht)
 {
     hash_node_t *ptr;
-    unsigned int i = 0, index = 0;
+    unsigned int index = 0;
+    int count, i = 0;
 
+    count = count_nodes(ht);
+    printf("{");
     if (ht != NULL)
     {
-        printf("{");
-        while (i < ht->size)
+        while (index < ht->size)
         {
             if (ht->array[index] != NULL)
             {
                 ptr = ht->array[index];
-                while (ptr != NULL)
-                {
-                    printf("'%s': '%s', ", ptr->key, ptr->value);
-                    ptr = ptr->next;
-                }
+                printf("'%s': '%s'", ptr->key, ptr->value);
+                i++;
+                if (i < count)
+                    printf(", ");
             }
-            else
-                continue;
-            i++;
             index++;
         }
-        printf("}");
     }
+    printf("}\n");
+}
+
+int check_for_another_node(unsigned int index, const hash_table_t *ht)
+{
+   while (ht->array[index] == NULL)
+   {
+        if (index < ht->size)
+            index++;
+        else
+        {
+            return (0);
+        }
+   }
+   return (1);
+}
+
+int count_nodes(const hash_table_t *ht)
+{
+    int node = 0, index;
+
+    for (index = 0; index < (int)ht->size; index++)
+    {
+        if (ht->array[index] != NULL)
+            node++;
+    }
+    return (node);
 }
