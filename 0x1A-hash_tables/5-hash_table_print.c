37fbff5ca5b3d3a8cdd26557dcbe5e7ae271a3b4
Idowu-David <idowudavidodun@gmail.com>
2022-09-24 10:43:23 +0000

    fix: handle keys with collision

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,52 +6,48 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *ptr;
-    unsigned int index = 0;
-    int count, i = 0;
+	hash_node_t *ptr;
+	unsigned int index = 0;
+	int count, i = 0;
 
-    count = count_nodes(ht);
-    printf("{");
-    if (ht != NULL)
-    {
-        while (index < ht->size)
-        {
-            if (ht->array[index] != NULL)
-            {
-                ptr = ht->array[index];
-                printf("'%s': '%s'", ptr->key, ptr->value);
-                i++;
-                if (i < count)
-                    printf(", ");
-            }
-            index++;
-        }
-    }
-    printf("}\n");
+	count = count_nodes(ht);
+	printf("{");
+	if (ht != NULL)
+	{
+		while (index < ht->size)
+		{
+			if (ht->array[index] != NULL)
+			{
+				ptr = ht->array[index];
+				while (ptr != NULL)
+				{
+					printf("'%s': '%s'", ptr->key, ptr->value);
+					ptr = ptr->next;
+				}
+
+			i++;
+			if (i < count)
+				printf(", ");
+			}
+			index++;
+		}
+	}
+	printf("}\n");
 }
 
-int check_for_another_node(unsigned int index, const hash_table_t *ht)
-{
-   while (ht->array[index] == NULL)
-   {
-        if (index < ht->size)
-            index++;
-        else
-        {
-            return (0);
-        }
-   }
-   return (1);
-}
-
+/**
+ * count_nodes - counts the number of nodes in the hash table
+ * @ht: hash node
+ * Return: the number of nodes
+ */
 int count_nodes(const hash_table_t *ht)
 {
-    int node = 0, index;
+	int node = 0, index;
 
-    for (index = 0; index < (int)ht->size; index++)
-    {
-        if (ht->array[index] != NULL)
-            node++;
-    }
-    return (node);
+	for (index = 0; index < (int)ht->size; index++)
+	{
+		if (ht->array[index] != NULL)
+			node++;
+	}
+	return (node);
 }
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned int index = 0;
+    hash_node_t *ptr, *temp;
+
+    if (ht == NULL)
+        return;
+    while (index < ht->size)
+    {
+        if (ht->array[index] != NULL)
+        {
+            temp = ptr = ht->array[index];
+            while (ptr != NULL)
+            {
+                ptr = ptr->next;
+            }
+                }
+    }
+}
