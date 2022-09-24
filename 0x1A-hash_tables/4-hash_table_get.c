f67e5e8c089353322a1e327304206ce501b65cdd
Idowu-David <idowudavidodun@gmail.com>
2022-09-24 08:14:54 +0000

    fix: get key from table with collision

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,6 +9,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned int index;
+	hash_node_t *ptr;
 	char *value;
 
 	if (key == NULL || ht == NULL)
@@ -17,6 +18,16 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht->array[index] == NULL)
 		value = NULL;
 	else
-		value = ht->array[index]->value;
+	{
+		ptr = ht->array[index];
+		while (ptr != NULL)
+		{
+			if (strcmp(ptr->key, key) == 0)
+			{
+				value = ptr->value;
+			}
+			ptr = ptr->next;
+		}
+	}
 	return (value);
 }
