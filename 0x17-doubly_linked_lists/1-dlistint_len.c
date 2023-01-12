fbac8e1160482bd6226e1ca19481d6a08df4c8bd
Idowu-David <idowudavidodun@gmail.com>
2023-01-13 00:48:29 +0100

    add: count number of nodes

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - counts the number of nodes in a dlistint
+ * @h: pointer to the first node
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
