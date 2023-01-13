e0a5c04d42f389b4a6a9e5f0c2532e46ca402812
Idowu-David <idowudavidodun@gmail.com>
2023-01-13 03:48:24 +0100

    add: sum lists

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -9,7 +9,9 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
-	while (head->next != NULL)
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
 	{
 		sum += head->n;
 		head = head->next;
