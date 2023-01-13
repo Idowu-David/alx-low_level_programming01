d5cfc97432e1f2919753b7d28d56021e10df0965
Idowu-David <idowudavidodun@gmail.com>
2023-01-13 11:54:47 +0100

    fix: space before parenthesis ln 19

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
-	if(ptr == NULL)
+	if (ptr == NULL)
 	{
 		new->prev = NULL;
 		(*head) = new;
