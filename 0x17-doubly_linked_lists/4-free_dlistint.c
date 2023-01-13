35eca331ab55c4a80e7d6d0513ff3deba7091b32
Idowu-David <idowudavidodun@gmail.com>
2023-01-13 01:31:07 +0100

    add:free list.

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t
+ * @head: pointer to the first node
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr, *curr;
+
+	if (head == NULL)
+		return;
+	ptr = head;
+	curr = ptr->next;
+	while (curr != NULL)
+	{
+		ptr->next = NULL;
+		curr->prev= NULL;
+		free(ptr);
+		ptr = curr;
+		curr = curr->next;
+	}
+}
