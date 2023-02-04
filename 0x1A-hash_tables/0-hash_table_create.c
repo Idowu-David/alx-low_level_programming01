d7423c9efdb9d2be0917d78a1903b54412b58860
Idowu-David <idowudavidodun@gmail.com>
2023-02-04 04:23:44 +0000

    fix: sise of hashnode

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
-	new_table = malloc(sizeof(hash_table_t) * size);
+	new_table = malloc(sizeof(hash_node_t) * size);
 	if (new_table == NULL)
 		return (NULL);
 	return (new_table);
